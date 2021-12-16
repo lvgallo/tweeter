90bfedfa02dfe4199a5168d1bfffc7981d4e33d4
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-12-16 06:35:05 +0000

    eslint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,31 +10,32 @@ $(document).ready(function() {
   const data = [
     {
       "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-},
-{
-  "user": {
-    "name": "Descartes",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
-    "handle": "@rd" },
-    "content": {
-    "text": "Je pense , donc je suis"
-  },
-  "created_at": 1461113959088
-}
-];
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
   
-const createTweetElement = function(data) {
-  const time = timeago.format(data.created_at);
-  const tweetElement = 
-  `<article class='tweet'>
+  const createTweetElement = function(data) {
+    const time = timeago.format(data.created_at);
+    const tweetElement =
+    `<article class='tweet'>
     <header>
     <div class="userLogo"> <img class="avatar" src="${data.user.avatars}"></img>
     <name>${data.user.name}</name>
@@ -54,7 +55,7 @@ const createTweetElement = function(data) {
     </article>
     `;
     return tweetElement;
-  }
+  };
 
   const renderTweets = function(data) {
     // loops through tweetss
@@ -64,7 +65,7 @@ const createTweetElement = function(data) {
       // takes return value and appends it to the tweets container
       $('#tweets-container').prepend(showTweet);
     }
-  }
+  };
      
   $('.compose-tweet').submit(function(event) {
     $(".error").css("display", "none");
@@ -76,35 +77,35 @@ const createTweetElement = function(data) {
       $(".error").text("Too long! Your post should have no more than 140 characters!").slideDown();
     } else {
       $.ajax({
-          url: '/tweets/',
-          method: 'POST',
-          data: dataSerialized
-          })
-        .done(function(){
-          $('#tweets-container').empty()
+        url: '/tweets/',
+        method: 'POST',
+        data: dataSerialized
+      })
+        .done(function() {
+          $('#tweets-container').empty();
           $('.compose-tweet').trigger("reset");
           $('.counter').text(140);
-        loadTweets()
-        })
+          loadTweets();
+        });
     }
-  })
+  });
 
-  const loadTweets = function () {
+  const loadTweets = function() {
     $.ajax({
       url: "/tweets/",
       method: "GET",
-    }).done(function (tweet) {
+    }).done(function(tweet) {
       renderTweets(tweet);
     });
   };
   
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  loadTweets()
+  loadTweets();
 });
 
   
